e97b142396afdf585056a79b1a81b4cae927c75c
mehrabc <mehrab367@gmail.com>
2025-05-21 15:55:42 +0600

    (feature): Added a feature to extract top keywords by tokenizing and removing the stop words

--- summarizer.py
+++ summarizer.py
@@ -4,6 +4,10 @@ import nltk
 # Force download of proper resources
 nltk.download('punkt')
 nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+import string
 
 # Part 1: Separating User and AI messages and storing inside distinct arrays
 def parse_chat_log(file_path):
@@ -37,7 +41,20 @@ def compute_message_statistics(user_messages, ai_messages):
         'user': len(user_messages),
         'ai': len(ai_messages)
     }
-    
+
+#Extracting top keywords by tokenizing the words and removing the stop words("I","you","the" etc)
+def extract_top_keywords(user_messages, ai_messages, top_n=5):
+    all_text = ' '.join(user_messages + ai_messages)
+    tokens = word_tokenize(all_text)
+    tokens = [word for word in tokens if word.isalnum()]
+
+
+    stop_words = set(stopwords.words('english'))
+    filtered_words = [word.lower() for word in tokens if word.lower() not in stop_words]
+    word_counts = Counter(filtered_words)
+
+    return word_counts.most_common(top_n)
+
 # Main function
 if __name__ == '__main__':
     import argparse
@@ -52,8 +69,10 @@ if __name__ == '__main__':
     if args.file:
         filename = os.path.basename(args.file)
         user_msgs, ai_msgs = parse_chat_log(args.file)
+
         print(f"\n📄 File: {filename}")
         print("👤 User Messages:")
+
         for msg in user_msgs:
             print(f"  - {msg}")
 
@@ -63,8 +82,13 @@ if __name__ == '__main__':
         
         exchange_count = compute_exchanges(user_msgs, ai_msgs)
         messages_count = compute_message_statistics(user_msgs, ai_msgs)
+        top_keywords = extract_top_keywords(user_msgs,ai_msgs)
+
         print(f"🔁 Number of Exchanges: {exchange_count}")
         print(f"🔁 Number of Messages: {messages_count}")
+        for word, count in top_keywords:
+            print(f"  - {word}")
+            
     # Testing if the program can segregate multiple files together
     elif args.folder:
         for filename in os.listdir(args.folder):
@@ -83,8 +107,12 @@ if __name__ == '__main__':
 
                 exchange_count = compute_exchanges(user_msgs, ai_msgs)
                 messages_count = compute_message_statistics(user_msgs, ai_msgs)
+                top_keywords = extract_top_keywords(user_msgs,ai_msgs)
                 print(f"🔁 Number of Exchanges: {exchange_count}")
                 print(f"🔁 Number of Messages: {messages_count}")
+                print("Top 5 Keywords:")
+                for word, count in top_keywords:
+                    print(f"  - {word}")
     else:
         print("Please provide a file to start summarizing.")
         
